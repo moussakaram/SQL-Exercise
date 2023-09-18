13f3b14a8fff79635b059bc864b4fecdd99fb3cb
moussakaram <moussakaram2@gmail.com>
2023-09-18 11:48:16 +0300

    2nd problem

---
+++ answers.sh
@@ -0,0 +1,10 @@
+Problem 0: SELECT * FROM students;
+Problem 1: SELECT name FROM students;
+Problem 2: SELECT * FROM students WHERE Age>30;
+Problem 3: SELECT Name FROM students WHERE Gender="F" AND Age=30;
+Problem 4: SELECT Points FROM students WHERE Name="Alex";
+Problem 5: INSERT INTO students (ID , name , Age, Gender , Points) VALUES( 7 ,"Moussa" , 33, "M", 700);
+Problem 6: SET Points = Points + 20 WHERE name = 'Basma';
+Problem 7: UPDATE students SET Points = Points  -20 WHERE name = 'Alex';
+
+
